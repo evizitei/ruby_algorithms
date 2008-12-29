973fd4163d63fd434a7409d4b0f5c4f4b0e0dbf4
Ethan Vizitei <ethan@ethan-viziteis-imac.local>
2008-12-29 21:36:26 +0000

    make harness use large set of random numbers to demonstrate difference between sorts

--- sorter_harness.rb
+++ sorter_harness.rb
@@ -1,16 +1,18 @@
 require "sorter"
 
 def test_sort(method)
+  size = 5000
   puts "---------------------------"
-  array = [9,3,9977,7,43,90,433,65,1,8974,76,3,755,900,39,432,8,2,12,54,23,134,67456,12,666,155,1290,80,199,22,22,906,673,236,1111]
+  array = []
+  size.times do |i|
+    array[i] = rand(size)
+  end
   puts "harnessing #{method}"
-  puts "opening array:"
-  puts array.join(",")
+  puts "opening array: #{array[0]} to #{array[size-1]}"
   start_time = Time.now
   array = Sorter.send(method,array)
   end_time = Time.now
-  puts "final array:"
-  puts array.join(",")
+  puts "final array: #{array[0]} to #{array[size-1]}"
   puts "time: #{end_time - start_time}"
   puts "---------------------------"
 end
