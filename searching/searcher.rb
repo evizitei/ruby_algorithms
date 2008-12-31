eeb0b339689badb138f67e11a64150a65dcc82ce
Ethan Vizitei <ethan@ethan-viziteis-imac.local>
2008-12-31 06:02:17 +0000

    restructure directories, and add searching algorithms

---
+++ searching/searcher.rb
@@ -0,0 +1,28 @@
+class Searcher
+  def self.naive_search(array,token,low,high)
+    for i in 0..(array.size - 1) do
+      value = array[i]
+      if (value > token)
+        return -1
+      elsif (value == token)
+        return i
+      end
+    end
+    return -1
+  end
+  
+  def self.binary_search(array,token,low,high)
+    diff = (high-low)
+    center_index = low + (diff / 2).to_i
+    center_value = array[center_index]
+    if(center_value == token)
+      center_index
+    elsif diff <= 1
+      return -1
+    elsif center_value > token
+      binary_search(array,token,low,center_index)
+    else
+      binary_search(array,token,center_index,high)
+    end
+  end
+end
---
+++ searching/searcher_harness.rb
@@ -0,0 +1,26 @@
+require "searcher"
+require "../sorting/sorter"
+
+SIZE = 20000
+ARRAY = []
+SIZE.times do |i|
+  ARRAY[i] = rand(SIZE)
+end
+ARRAY = Sorter.merge_sort(ARRAY)
+TOKEN = rand(SIZE)
+
+def test_search(method)
+  puts "---------------------------"
+  puts "harnessing #{method}"
+  puts "searching for #{TOKEN}"
+  start_time = Time.now
+  index = Searcher.send(method,ARRAY,TOKEN,0,SIZE - 1)
+  end_time = Time.now
+  puts "found at index: #{index}"
+  puts "time: #{end_time - start_time}"
+  puts "---------------------------"
+end
+
+puts "------Searcher Harness----------"
+test_search(:naive_search)
+test_search(:binary_search)
